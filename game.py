06403e2b7ba8992b8b6487028cf8dab08a8b8e6f
Kirill-Kuznetsov-git <kir.cuzneczo2012@yandex.ru>
2020-02-07 14:36:02 +0400

    die

--- game.py
+++ game.py
@@ -2,10 +2,10 @@ import pygame
 import os
 
 
-width = 700
-height = 500
+width = 600
+height = 510
 fps = 15
-GRAVITY = 15
+GRAVITY = 21
 coords = [0, 0]
 motion = 'stop'
 clock = pygame.time.Clock()
@@ -34,15 +34,14 @@ def load_level(filename):
     # и подсчитываем максимальную длину
     max_width = max(map(len, level_map))
 
-    # дополняем каждую строку пустыми клетками ('.')
+    #дополняем каждую строку пустыми клетками ('.')
     return list(map(lambda x: x.ljust(max_width, '.'), level_map))
 
 
-tile_images = {'ground': load_image('ground.png', -1),
+tile_images = {'ground': load_image('ground0.png', -1),
                'sprikes': load_image('sprikes.png', -1),
                'star': load_image('star.png', -1)}
 player_image = {'run_r': load_image('run_r.png', -1), 'run_l': load_image('run_l.png', -1),
-                'jump_r': load_image('jump_r.png', -1), 'jump_l': load_image('jump_l.png', -1),
                 'shoot': load_image('shoot.png', -1), 'die': load_image('die.png', -1),
                 'stay': load_image('stay.png', -1)}
 tile_width = 30
@@ -63,7 +62,8 @@ class Player(pygame.sprite.Sprite):
         self.cur_frame = 0
         self.stay = False
         self.jump = False
-        self.force = 400
+        self.die = False
+        self.force = 10
         self.image = self.frames[self.cur_frame]
         self.rect = self.rect.move(tile_width * pos_x + 15, tile_height * pos_y + 5)
 
@@ -76,64 +76,60 @@ class Player(pygame.sprite.Sprite):
                 self.frames.append(sheet.subsurface(pygame.Rect(
                     frame_location, self.rect.size)))
 
-    def update(self):
+    def update(self, *args):
+        if load_level('level_1.txt')[int(coords[1] // 30) - 1][int(coords[0] // 30)] == '@':
+            print(load_level('level_1.txt')[int(coords[1] // 30) - 1][int(coords[0] // 30)])
         for j in pygame.sprite.spritecollide(self, tiles_group, False):
             if j.tile_type == 'ground':
                 self.stay = True
             elif j.tile_type == 'sprikes':
-                pass
+                self.die = True
         if len(pygame.sprite.spritecollide(self, tiles_group, False)) == 0:
-                self.stay = False
+            self.stay = False
+
         if self.stay is False:
             coords[1] += GRAVITY
+            self.rect.y += GRAVITY
 
-        if i.type == pygame.KEYDOWN:
+        if i.type == pygame.KEYDOWN and self.die is False:
             if i.key == pygame.K_SPACE and self.stay is True:
                 self.jump = True
-                if motion == 'right' or motion == 'stay':
-                    self.cut_sheet(player_image['jump_r'], 5, 1)
-                elif motion == 'left':
-                    self.cut_sheet(player_image['jump_l'], 5, 1)
-            if i.key == pygame.K_LEFT:
-                self.frames = []
-                self.cut_sheet(player_image['run_l'], 7, 1)
-            elif i.key == pygame.K_RIGHT:
-                self.frames = []
-                self.cut_sheet(player_image['run_r'], 7, 1)
-        else:
+
+        if args[0] is True and motion == 'left' and self.die is False:
+            self.frames = []
+            self.cut_sheet(player_image['run_l'], 7, 1)
+        elif args[0] is True and motion == 'right' and self.die is False:
+            self.frames = []
+            self.cut_sheet(player_image['run_r'], 7, 1)
+        elif args[0] is True and motion == 'stop' and self.die is False:
             self.frames = []
             self.cut_sheet(player_image['stay'], 1, 1)
 
-        if self.jump is True:
+        if self.jump is True and self.die is False:
             if self.force > 0:
-                self.force -= 100
-                self.rect.y -= 30
-                coords[1] -= 30
+                if self.force >= 0:
+                    self.rect.y -= (self.force ** 2) / 2
+                    coords[1] -= (self.force ** 2) / 2
+                else:
+                    self.rect.y += (self.force ** 2) / 2
+                    coords[1] += (self.force ** 2) / 2
+                self.force -= 1
             else:
                 self.jump = False
-                self.force = 400
-                if motion == 'left':
-                    self.frames = []
-                    self.cut_sheet(player_image['run_l'], 7, 1)
-                elif motion == 'right':
-                    self.frames = []
-                    self.cut_sheet(player_image['run_r'], 7, 1)
-            self.cur_frame = (self.cur_frame + 1) % len(self.frames)
-            self.image = self.frames[self.cur_frame]
+                self.force = 10
 
-        if motion == 'stop':
-            self.cur_frame = (self.cur_frame + 1) % len(self.frames)
-            self.image = self.frames[self.cur_frame]
-        elif motion == 'left':
+        if motion == 'left':
             coords[0] -= 5
             self.rect.x -= 5
-            self.cur_frame = (self.cur_frame + 1) % len(self.frames)
-            self.image = self.frames[self.cur_frame]
         elif motion == 'right':
             coords[0] += 5
             self.rect.x += 5
-            self.cur_frame = (self.cur_frame + 1) % len(self.frames)
-            self.image = self.frames[self.cur_frame]
+        self.cur_frame = (self.cur_frame + 1) % len(self.frames)
+        self.image = self.frames[self.cur_frame]
+
+        if self.die is True:
+            self.frames = []
+            self.cut_sheet(player_image['die'], 5, 1)
 
 
 class Tile(pygame.sprite.Sprite):
@@ -169,23 +165,32 @@ player, level_x, level_y = generate_level(load_level('level_1.txt'))
 fon = pygame.transform.scale(load_image('fon.png'), (width, height))
 screen.blit(fon, (0, 0))
 while running:
+    up = False
     for i in pygame.event.get():
         if i.type == pygame.QUIT:
             running = False
         elif i.type == pygame.KEYDOWN:
             if i.key == pygame.K_LEFT:
                 motion = 'left'
+                player_group.update(True)
+                up = True
             elif i.key == pygame.K_RIGHT:
                 motion = 'right'
+                player_group.update(True)
+                up = True
         elif i.type == pygame.KEYUP:
             if i.key in [pygame.K_LEFT, pygame.K_RIGHT]:
                 motion = 'stop'
+                player_group.update(True)
+                up = True
+    if up is False:
+        player_group.update(False)
     screen.blit(fon, (0, 0))
-    # изменяем ракурс камеры
-    # обновляем положение всех спрайтов
     for sprite in all_sprites:
-        sprite.rect.x -= 3
-    player_group.update()
+        sprite.rect.x -= 1
+    for sprite in player_group:
+        sprite.rect.x -= 1
+        coords[0] -= 1
     all_sprites.draw(screen)
     player_group.draw(screen)
     clock.tick(fps)
